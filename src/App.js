a8c743388d07d583852b0a60963edac6302b643c
RamonBR16 <70599806+RamonBR16@users.noreply.github.com>
2020-09-28 16:45:50 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
 import React from 'react';
 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         <h1>Site em construção</h1>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function app(){
+   return(
+      <h1>hello word!!! </h1>
+   );
+
 }
 
 export default App;
